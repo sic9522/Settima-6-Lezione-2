c5c2ba543becfb13917b542d930f9cd032ccc466
simone pierantozzi <pierantozzisimone@gmail.com>
2026-06-09 15:58:28 +0200

    finish

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+const carelloAdd = document.getElementById('carelloAdd')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+carello.addEventListener('click', () => {
+
+})
